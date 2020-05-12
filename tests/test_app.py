8874bf650632c7d43dbd33d95edffee6febdb83a
liamcryan <limecrayon@gmail.com>
2020-05-12 00:02:55 -0700

    update test for users

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,21 @@
-from app.models import Problem
+from app.models import Problem, User
 
 
 def test_get_empty(client):
-    rv = client.get('/problems')
+    rv = client.get('/')
     assert rv.json == []
 
 
 def test_post(client, problems, app):
-    rv = client.post('/problems', json=problems)
+    rv = client.post('/', json=problems)
     assert rv.json == {}
 
     with app.app_context():
+        _user = User.query.first()
+        assert _user.name == None
+
         _problems = Problem.query.all()
+        assert len(_problems) == len(problems)
         for i, j in zip(_problems, problems):
             assert i.id == j['ID']
+            assert i.code == []
