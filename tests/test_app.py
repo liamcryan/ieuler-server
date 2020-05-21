d1d5825f8731101a268be90034de96370b333df7
liamcryan <limecrayon@gmail.com>
2020-05-20 21:07:07 -0700

    fix issue with getting wrong problems & add test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,21 @@
+import json
 from app.models import Problem, User
 
 
 def test_get_empty(client):
     rv = client.get('/api/problems')
     assert rv.json == []
+
+
+def test_get_user_data(client):
+    client.post('/api/problems', json=[{"ID": 2, "Description / Title": "Even Fibonacci numbers", "Solved By": "750055",
+                                        "problem_url": "https://projecteuler.net/problem=2",
+                                        "page_url": "https://projecteuler.net/archives",
+                                        "code": {"python3": {"filename": "2.py", "filecontent": "asdf",
+                                                             "submission": None}}}])
+    r = client.get('/api/problems')
+    assert r.json == [{"ID": 2, "Solved": None, "completed_on": None, "correct_answer": None,
+                       "code": {"python3": {"filename": "2.py", "filecontent": "asdf", "submission": None}}}]
 
 
 def test_post(client, problems, app):
